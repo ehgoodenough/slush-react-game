00fd2711f42c6de6e1440c4771f1ed00082c84d3
Andrew McPherson <andrewmcp333@gmail.com>
2015-03-07 15:16:18 -0800

    Added gitignore to the template with underscores.

--- slushfile.js
+++ slushfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp")
 var gulp_install = require("gulp-install")
 var gulp_conflict = require("gulp-conflict")
 var gulp_template = require("gulp-template")
+var gulp_rename = require("gulp-rename")
 
 var path = require("path")
 var inquirer = require("inquirer")
@@ -23,6 +24,11 @@ gulp.task("default", function(done) {
         if(answers.confirmed) {
             gulp.src(path.join(__dirname, "./template/**/*"))
                 .pipe(gulp_template(answers))
+                .pipe(gulp_rename(function(file) {
+                    if(file.basename.charAt(0) === "_") {
+                        file.basename = "." + file.basename.slice(1)
+                    }
+                }))
                 .pipe(gulp_conflict(__dirname + "/temp"))
                 .pipe(gulp.dest(__dirname + "/temp"))
                 .pipe(gulp_install())
